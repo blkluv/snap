97d4da0252bac02a1367d2aed299f9f73152009a
Ryan <81343914+McOso@users.noreply.github.com>
2023-03-13 19:49:58 -0400

    Initial commit

---
+++ integrations/disco/get-profile-from-address.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+import { discoAPI } from '@/integrations/disco/disco-api'
+
+export async function discoGetProfileFromAddress(address?: string): Promise<any> {
+  if (!address) {
+    return null
+  }
+  const { data } = await discoAPI.get(`/profile/address/${address}`)
+  return data
+}
+
+export async function appDiscoGetProfileFromAddress(address?: `0x${string}`): Promise<any> {
+  try {
+    const { data } = await axios.get(`/api/disco/profile-from-address`, {
+      params: {
+        address: address,
+      },
+    })
+    return data
+  } catch (error: any) {
+    throw error
+  }
+}
