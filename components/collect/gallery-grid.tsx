358b9c30f6cccb920ed021e2c87a268ed4a29c6c
Ryan McPeck <mcoso.dev@gmail.com>
2023-03-16 17:43:14 -0400

    fix: quick fix for improper image creation

--- components/collect/gallery-grid.tsx
+++ components/collect/gallery-grid.tsx
@@ -10,6 +10,7 @@ export default function GalleryGrid({ snaps }: GalleryGridProps) {
   return (
     <div className="flex flex-col rounded-md bg-neutral-100 text-center dark:bg-neutral-800">
       {snaps.map((snapAddress, index) => {
+        if (snapAddress == '0x98E2f4112E1d1CCd84f3FA77E6b229d9962fC275') return
         return (
           <div key={`collect-snap-${index}`} className="mb-10">
             <GalleryItem snap={snapAddress} />
